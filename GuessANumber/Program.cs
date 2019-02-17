019b7d0519a249e6e8e413dc893689e7662c9fe7
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-17 16:18:02 -0600

    Homework Finished

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -8,9 +8,9 @@ namespace GuessANumber
         {
             Console.Write("Enter a number between 1 and 10: ");
             int number = int.Parse(Console.ReadLine());
-
+            string message = (number == 8) ? "You Win!" : "You Lose";
             // If they enter 8, output “You Win!”. Otherwise, output “You Lose.”. Do this using an inline if statement.
-
+            Console.WriteLine(message);
             Console.ReadLine();
         }
     }
