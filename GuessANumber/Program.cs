aae6cccb88bb549db14fba429cae8f92c38d5f82
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-17 20:05:48 -0600

    added a period after "You Lose" to make it pass all tests

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -8,16 +8,12 @@ namespace GuessANumber
         {
             Console.Write("Enter a number between 1 and 10: ");
             int number = int.Parse(Console.ReadLine());
-            /*string message;
-            if (number == 8)
-            {
-                message = "You Win!";
-            }
-            else message = "You Lose!";*/
-            string message = (number == 8) ? "You Win!" : "You Lose!";
-         
+                       
+            string message = (number == 8) ? "You Win!" : "You Lose.";
+
+           
             // If they enter 8, output “You Win!”. Otherwise, output “You Lose”. Do this using an inline if statement.
-            Console.WriteLine(message);
+            Console.WriteLine(message);          
             Console.ReadLine();
         }
     }
